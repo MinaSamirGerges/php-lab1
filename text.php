c13b1a04db87c0711bf3660c3b6338b3a9a736bc
MinaSamir <minasamirgerguis84@gmail.com>
2023-03-25 14:41:46 +0200

    finaaal

--- text.php
+++ text.php
@@ -1,8 +1,12 @@
 <?php
-phpinfo();
+ phpinfo();
 
 
-print_r( $_SERVER );
+echo $_SERVER['SERVER_NAME']."<br>";
+
+echo $_SERVER['SERVER_ADDR']."<br>";
+
+echo $_SERVER['SERVER_PORT']."<br>";
 
 const WEB_NAME ='iti.com';
 echo WEB_NAME."<br>";
@@ -14,13 +18,14 @@ echo "the filename and path:".__FILE__;
 
 $age=10;
 $grade = 12-$age;
+
 switch ($age) {
 
     case "$age<5":
       echo "stay at home";
       break;
     case "$age=5":
-      echo "Your favorite color is blue!";
+      echo "go to kindergarden";
       break;
     case "$age>6 && $age <12":
        
