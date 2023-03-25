c2f7bdc6f58a8256f474f9b226582e84ce8254fa
MinaSamir <minasamirgerguis84@gmail.com>
2023-03-25 14:15:29 +0200

    lab1 - php -first trial

---
+++ text.php
@@ -0,0 +1,31 @@
+<?php
+phpinfo();
+
+
+print_r( $_SERVER );
+
+const WEB_NAME ='iti.com';
+echo WEB_NAME.<br>;
+
+
+
+
+echo "the filename and path:".__FILE__;
+
+$age=10;
+$grade = 12-$age:
+switch ($age) {
+
+    case "$age<5":
+      echo "stay at home";
+      break;
+    case "$age=5":
+      echo "Your favorite color is blue!";
+      break;
+    case "$age>6 && $age <12":
+       
+      echo "go to grade".$grade;
+      break;
+}
+
+?>
