cb1d2b4e56c23cb01cb95e5b943b62c0d8315c93
MinaSamir <minasamirgerguis84@gmail.com>
2023-03-25 14:48:31 +0200

    final2

--- text.php
+++ text.php
@@ -1,33 +1,33 @@
 <?php
- phpinfo();
+//  phpinfo();
 
 
-echo $_SERVER['SERVER_NAME']."<br>";
+// echo $_SERVER['SERVER_NAME']."<br>";
 
-echo $_SERVER['SERVER_ADDR']."<br>";
+// echo $_SERVER['SERVER_ADDR']."<br>";
 
-echo $_SERVER['SERVER_PORT']."<br>";
+// echo $_SERVER['SERVER_PORT']."<br>";
 
-const WEB_NAME ='iti.com';
-echo WEB_NAME."<br>";
-
-
+// const WEB_NAME ='iti.com';
+// echo WEB_NAME."<br>";
 
 
-echo "the filename and path:".__FILE__;
+
+
+echo "the filename and path:".__FILE__ ."<br>";
 
 $age=10;
 $grade = 12-$age;
 
 switch ($age) {
 
-    case "$age<5":
+    case $age<5:
       echo "stay at home";
       break;
-    case "$age=5":
+    case $age==5:
       echo "go to kindergarden";
       break;
-    case "$age>6 && $age <12":
+    case $age>6 && $age <12:
        
       echo "go to grade".$grade;
       break;
